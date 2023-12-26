5e32ae0129dc34f76f009fd5c652148b94620eae
Andry <useruseruser2232323@gmail.com>
2023-12-26 11:18:55 +0300

    lab13

--- labs/Lab_13_python_oop/solution/writer.py
+++ labs/Lab_13_python_oop/solution/writer.py
@@ -1,29 +1,23 @@
 import xlsxwriter
-from datetime import datetime
-from base import BaseXlsBlock
-from blocks import TopPayersBlock, TopCitiesBlock, AccountStatusBlock
-
+from block import Parameters,Report,Geography,Status
 class XlsAnalyticPaymentWriter:
     ANALYTICS_BLOCKS_CLASSES = [
-        TopPayersBlock,
-        TopCitiesBlock,
-        AccountStatusBlock
+        Parameters,
+        Report,
+        Geography,
+        Status
     ]
-
-    def __init__(self, data):
-        self.data = data
-
-    def write_excel_report(self, output_file):
-        workbook = xlsxwriter.Workbook(output_file)
+    def __init__(self, some_data):
+        self.data = some_data
+        self.position = 0
+    def writer(self, write_file):
+        workbook = xlsxwriter.Workbook(write_file)
         worksheet = workbook.add_worksheet()
-
+        worksheet.set_column('A:Z', 35)
         row = 0
         col = 0
-
-        for block_class in self.ANALYTICS_BLOCKS_CLASSES:
-            block_instance = block_class(worksheet, row, col, self.data)
-            block_instance.write_header()
-            block_instance.write_data()
-            row += 12
-
+        for items in self.ANALYTICS_BLOCKS_CLASSES:
+            item_init = items(workbook,worksheet, row, col, self.data)
+            item_init.writer_header()
+            item_init.writer_some_data()
         workbook.close()
